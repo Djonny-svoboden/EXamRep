f01b6a7b1d8983c9ce89b5b144836ccd14619b4d
Pikabu <57330062+Djonny-svoboden@users.noreply.github.com>
2020-12-18 11:44:10 +0200

    Update TestProgr.py

--- TestProgr.py
+++ TestProgr.py
@@ -5,7 +5,7 @@ class CalcTest(ut.TestCase):
     def setUp(self):
         self.mk=pt.MyClass()
     def test_add(self):
-        self.assertEqual(self.mk.Return_k(self.mk.Progression(1,2,3),3),8)
+        self.assertEqual(self.mk.Return_k(self.mk.Progression(1,3,5),5),50)
         
 if __name__ == '__main__':
     import xmlrunner
